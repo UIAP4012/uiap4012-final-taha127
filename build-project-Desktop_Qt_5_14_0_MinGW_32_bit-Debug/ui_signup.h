722822f49fbff318bb0e33f7893c2190a885186b
taha <tahadehghani127@gmail.com>
2023-06-24 12:29:11 +0330

    Adding project execution files

---
+++ build-project-Desktop_Qt_5_14_0_MinGW_32_bit-Debug/ui_signup.h
@@ -0,0 +1,430 @@
+/********************************************************************************
+** Form generated from reading UI file 'signup.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_SIGNUP_H
+#define UI_SIGNUP_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
+#include <QtWidgets/QSpinBox>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_signup
+{
+public:
+    QAction *actionback_to_log_in;
+    QAction *actionexit;
+    QAction *actionspace;
+    QAction *actionspace_2;
+    QAction *actionspace_3;
+    QAction *actionspace_4;
+    QAction *actionspace_5;
+    QAction *actionspace_6;
+    QAction *actionspace_7;
+    QAction *actionspace_8;
+    QAction *actionspace_9;
+    QAction *actionspace_10;
+    QAction *actionspace_11;
+    QAction *actionspace_12;
+    QAction *actionspace_13;
+    QWidget *centralwidget;
+    QFrame *frame;
+    QVBoxLayout *verticalLayout;
+    QLineEdit *lineEdit;
+    QLineEdit *lineEdit_2;
+    QSpinBox *spinBox;
+    QLineEdit *lineEdit_3;
+    QLineEdit *lineEdit_4;
+    QHBoxLayout *horizontalLayout;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+    QToolBar *toolBar;
+
+    void setupUi(QMainWindow *signup)
+    {
+        if (signup->objectName().isEmpty())
+            signup->setObjectName(QString::fromUtf8("signup"));
+        signup->resize(514, 465);
+        signup->setStyleSheet(QString::fromUtf8("background-color: qlineargradient(spread:reflect, x1:0, y1:0, x2:1, y2:1, stop:0 rgba(41, 73, 185, 255), stop:0.865169 rgba(255, 39, 39, 255));"));
+        actionback_to_log_in = new QAction(signup);
+        actionback_to_log_in->setObjectName(QString::fromUtf8("actionback_to_log_in"));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/resources/image/2b05.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionback_to_log_in->setIcon(icon);
+        actionexit = new QAction(signup);
+        actionexit->setObjectName(QString::fromUtf8("actionexit"));
+        QIcon icon1;
+        icon1.addFile(QString::fromUtf8(":/resources/image/closewindow_hover.png"), QSize(), QIcon::Normal, QIcon::Off);
+        actionexit->setIcon(icon1);
+        actionexit->setMenuRole(QAction::NoRole);
+        actionspace = new QAction(signup);
+        actionspace->setObjectName(QString::fromUtf8("actionspace"));
+        actionspace->setEnabled(false);
+        actionspace->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        QFont font;
+        font.setPointSize(33);
+        font.setUnderline(false);
+        font.setStrikeOut(false);
+        font.setKerning(false);
+        actionspace->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace->setAutoRepeat(false);
+        actionspace->setVisible(true);
+        actionspace->setIconVisibleInMenu(false);
+        actionspace->setShortcutVisibleInContextMenu(false);
+        actionspace_2 = new QAction(signup);
+        actionspace_2->setObjectName(QString::fromUtf8("actionspace_2"));
+        actionspace_2->setEnabled(false);
+        actionspace_2->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_2->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_2->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_2->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_2->setAutoRepeat(false);
+        actionspace_2->setVisible(true);
+        actionspace_2->setIconVisibleInMenu(false);
+        actionspace_2->setShortcutVisibleInContextMenu(false);
+        actionspace_3 = new QAction(signup);
+        actionspace_3->setObjectName(QString::fromUtf8("actionspace_3"));
+        actionspace_3->setEnabled(false);
+        actionspace_3->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_3->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_3->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_3->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_3->setAutoRepeat(false);
+        actionspace_3->setVisible(true);
+        actionspace_3->setIconVisibleInMenu(false);
+        actionspace_3->setShortcutVisibleInContextMenu(false);
+        actionspace_4 = new QAction(signup);
+        actionspace_4->setObjectName(QString::fromUtf8("actionspace_4"));
+        actionspace_4->setEnabled(false);
+        actionspace_4->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_4->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_4->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_4->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_4->setAutoRepeat(false);
+        actionspace_4->setVisible(true);
+        actionspace_4->setIconVisibleInMenu(false);
+        actionspace_4->setShortcutVisibleInContextMenu(false);
+        actionspace_5 = new QAction(signup);
+        actionspace_5->setObjectName(QString::fromUtf8("actionspace_5"));
+        actionspace_5->setEnabled(false);
+        actionspace_5->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_5->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_5->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_5->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_5->setAutoRepeat(false);
+        actionspace_5->setVisible(true);
+        actionspace_5->setIconVisibleInMenu(false);
+        actionspace_5->setShortcutVisibleInContextMenu(false);
+        actionspace_6 = new QAction(signup);
+        actionspace_6->setObjectName(QString::fromUtf8("actionspace_6"));
+        actionspace_6->setEnabled(false);
+        actionspace_6->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_6->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_6->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_6->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_6->setAutoRepeat(false);
+        actionspace_6->setVisible(true);
+        actionspace_6->setIconVisibleInMenu(false);
+        actionspace_6->setShortcutVisibleInContextMenu(false);
+        actionspace_7 = new QAction(signup);
+        actionspace_7->setObjectName(QString::fromUtf8("actionspace_7"));
+        actionspace_7->setEnabled(false);
+        actionspace_7->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_7->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_7->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_7->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_7->setAutoRepeat(false);
+        actionspace_7->setVisible(true);
+        actionspace_7->setIconVisibleInMenu(false);
+        actionspace_7->setShortcutVisibleInContextMenu(false);
+        actionspace_8 = new QAction(signup);
+        actionspace_8->setObjectName(QString::fromUtf8("actionspace_8"));
+        actionspace_8->setEnabled(false);
+        actionspace_8->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_8->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_8->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_8->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_8->setAutoRepeat(false);
+        actionspace_8->setVisible(true);
+        actionspace_8->setIconVisibleInMenu(false);
+        actionspace_8->setShortcutVisibleInContextMenu(false);
+        actionspace_9 = new QAction(signup);
+        actionspace_9->setObjectName(QString::fromUtf8("actionspace_9"));
+        actionspace_9->setEnabled(false);
+        actionspace_9->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_9->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_9->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_9->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_9->setAutoRepeat(false);
+        actionspace_9->setVisible(true);
+        actionspace_9->setIconVisibleInMenu(false);
+        actionspace_9->setShortcutVisibleInContextMenu(false);
+        actionspace_10 = new QAction(signup);
+        actionspace_10->setObjectName(QString::fromUtf8("actionspace_10"));
+        actionspace_10->setEnabled(false);
+        actionspace_10->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_10->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_10->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_10->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_10->setAutoRepeat(false);
+        actionspace_10->setVisible(true);
+        actionspace_10->setIconVisibleInMenu(false);
+        actionspace_10->setShortcutVisibleInContextMenu(false);
+        actionspace_11 = new QAction(signup);
+        actionspace_11->setObjectName(QString::fromUtf8("actionspace_11"));
+        actionspace_11->setEnabled(false);
+        actionspace_11->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_11->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_11->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_11->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_11->setAutoRepeat(false);
+        actionspace_11->setVisible(true);
+        actionspace_11->setIconVisibleInMenu(false);
+        actionspace_11->setShortcutVisibleInContextMenu(false);
+        actionspace_12 = new QAction(signup);
+        actionspace_12->setObjectName(QString::fromUtf8("actionspace_12"));
+        actionspace_12->setEnabled(false);
+        actionspace_12->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_12->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_12->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_12->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_12->setAutoRepeat(false);
+        actionspace_12->setVisible(true);
+        actionspace_12->setIconVisibleInMenu(false);
+        actionspace_12->setShortcutVisibleInContextMenu(false);
+        actionspace_13 = new QAction(signup);
+        actionspace_13->setObjectName(QString::fromUtf8("actionspace_13"));
+        actionspace_13->setEnabled(false);
+        actionspace_13->setIconText(QString::fromUtf8(""));
+#if QT_CONFIG(tooltip)
+        actionspace_13->setToolTip(QString::fromUtf8(""));
+#endif // QT_CONFIG(tooltip)
+        actionspace_13->setFont(font);
+#if QT_CONFIG(shortcut)
+        actionspace_13->setShortcut(QString::fromUtf8(""));
+#endif // QT_CONFIG(shortcut)
+        actionspace_13->setAutoRepeat(false);
+        actionspace_13->setVisible(true);
+        actionspace_13->setIconVisibleInMenu(false);
+        actionspace_13->setShortcutVisibleInContextMenu(false);
+        centralwidget = new QWidget(signup);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        frame = new QFrame(centralwidget);
+        frame->setObjectName(QString::fromUtf8("frame"));
+        frame->setGeometry(QRect(90, 40, 341, 321));
+        frame->setStyleSheet(QString::fromUtf8("color: rgb(123, 230, 50);\n"
+"background-color: rgb(85, 0, 127);\n"
+"font: 11pt \"Segoe UI Symbol\";\n"
+"border-radius : 8px;\n"
+""));
+        frame->setFrameShape(QFrame::StyledPanel);
+        frame->setFrameShadow(QFrame::Raised);
+        verticalLayout = new QVBoxLayout(frame);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        lineEdit = new QLineEdit(frame);
+        lineEdit->setObjectName(QString::fromUtf8("lineEdit"));
+        lineEdit->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+
+        verticalLayout->addWidget(lineEdit);
+
+        lineEdit_2 = new QLineEdit(frame);
+        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        lineEdit_2->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+
+        verticalLayout->addWidget(lineEdit_2);
+
+        spinBox = new QSpinBox(frame);
+        spinBox->setObjectName(QString::fromUtf8("spinBox"));
+        spinBox->setCursor(QCursor(Qt::PointingHandCursor));
+        spinBox->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+        spinBox->setMinimum(15);
+        spinBox->setMaximum(100);
+
+        verticalLayout->addWidget(spinBox);
+
+        lineEdit_3 = new QLineEdit(frame);
+        lineEdit_3->setObjectName(QString::fromUtf8("lineEdit_3"));
+        lineEdit_3->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+
+        verticalLayout->addWidget(lineEdit_3);
+
+        lineEdit_4 = new QLineEdit(frame);
+        lineEdit_4->setObjectName(QString::fromUtf8("lineEdit_4"));
+        lineEdit_4->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+        lineEdit_4->setEchoMode(QLineEdit::Password);
+
+        verticalLayout->addWidget(lineEdit_4);
+
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        radioButton = new QRadioButton(frame);
+        radioButton->setObjectName(QString::fromUtf8("radioButton"));
+        radioButton->setCursor(QCursor(Qt::PointingHandCursor));
+        radioButton->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+
+        horizontalLayout->addWidget(radioButton);
+
+        radioButton_2 = new QRadioButton(frame);
+        radioButton_2->setObjectName(QString::fromUtf8("radioButton_2"));
+        radioButton_2->setCursor(QCursor(Qt::PointingHandCursor));
+        radioButton_2->setStyleSheet(QString::fromUtf8("background-color: rgb(170, 85, 127);"));
+
+        horizontalLayout->addWidget(radioButton_2);
+
+
+        verticalLayout->addLayout(horizontalLayout);
+
+        pushButton = new QPushButton(frame);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setCursor(QCursor(Qt::PointingHandCursor));
+        pushButton->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 0, 0);\n"
+""));
+
+        verticalLayout->addWidget(pushButton);
+
+        signup->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(signup);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 514, 21));
+        signup->setMenuBar(menubar);
+        statusbar = new QStatusBar(signup);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        signup->setStatusBar(statusbar);
+        toolBar = new QToolBar(signup);
+        toolBar->setObjectName(QString::fromUtf8("toolBar"));
+        toolBar->setLayoutDirection(Qt::LeftToRight);
+        toolBar->setStyleSheet(QString::fromUtf8("background-color: rgb(119, 54, 231);"));
+        toolBar->setMovable(false);
+        signup->addToolBar(Qt::TopToolBarArea, toolBar);
+
+        toolBar->addAction(actionback_to_log_in);
+        toolBar->addAction(actionspace);
+        toolBar->addAction(actionspace_3);
+        toolBar->addAction(actionspace_4);
+        toolBar->addAction(actionspace_5);
+        toolBar->addAction(actionspace_6);
+        toolBar->addAction(actionspace_7);
+        toolBar->addAction(actionspace_8);
+        toolBar->addAction(actionspace_9);
+        toolBar->addAction(actionspace_10);
+        toolBar->addAction(actionspace_11);
+        toolBar->addAction(actionspace_12);
+        toolBar->addAction(actionspace_13);
+        toolBar->addAction(actionspace_2);
+        toolBar->addAction(actionexit);
+
+        retranslateUi(signup);
+
+        QMetaObject::connectSlotsByName(signup);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *signup)
+    {
+        signup->setWindowTitle(QCoreApplication::translate("signup", "MainWindow", nullptr));
+        actionback_to_log_in->setText(QCoreApplication::translate("signup", "back to log in", nullptr));
+        actionexit->setText(QCoreApplication::translate("signup", "exit", nullptr));
+        actionspace->setText(QString());
+        actionspace_2->setText(QString());
+        actionspace_3->setText(QString());
+        actionspace_4->setText(QString());
+        actionspace_5->setText(QString());
+        actionspace_6->setText(QString());
+        actionspace_7->setText(QString());
+        actionspace_8->setText(QString());
+        actionspace_9->setText(QString());
+        actionspace_10->setText(QString());
+        actionspace_11->setText(QString());
+        actionspace_12->setText(QString());
+        actionspace_13->setText(QString());
+        lineEdit->setPlaceholderText(QCoreApplication::translate("signup", "name", nullptr));
+        lineEdit_2->setPlaceholderText(QCoreApplication::translate("signup", "family", nullptr));
+        spinBox->setSuffix(QCoreApplication::translate("signup", " year", nullptr));
+        spinBox->setPrefix(QString());
+        lineEdit_3->setPlaceholderText(QCoreApplication::translate("signup", "username", nullptr));
+        lineEdit_4->setPlaceholderText(QCoreApplication::translate("signup", "password", nullptr));
+        radioButton->setText(QCoreApplication::translate("signup", "Customer", nullptr));
+        radioButton_2->setText(QCoreApplication::translate("signup", "Seller", nullptr));
+        pushButton->setText(QCoreApplication::translate("signup", "sing up", nullptr));
+        toolBar->setWindowTitle(QCoreApplication::translate("signup", "toolBar", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class signup: public Ui_signup {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_SIGNUP_H
